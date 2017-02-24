2d3a83485bf6ac9140dc2e47a7358e6601dc15b5
Konstantin Androsov <konstantin.androsov@gmail.com>
2017-02-24 11:17:45 +0100

    Added function to compute whitening function for random variables.
    Other changes:
    - applied whitening for DY sf unc in ttbb.
    - minor modifications in CommonUncertainties.h

--- Run2_2016/src/ttbb.cc
+++ Run2_2016/src/ttbb.cc
@@ -2,7 +2,6 @@
 This file is part of https://github.com/cms-hh/HHStatAnalysis. */
 
 #include <iostream>
-#include <Math/SMatrix.h>
 #include "CombineHarvester/CombineTools/interface/Systematics.h"
 #include "CombineHarvester/CombineTools/interface/CardWriter.h"
 #include "CombineHarvester/CombinePdfs/interface/MorphFunctions.h"
@@ -12,6 +11,7 @@ This file is part of https://github.com/cms-hh/HHStatAnalysis. */
 #include "HHStatAnalysis/Core/interface/TextIO.h"
 #include "HHStatAnalysis/Core/interface/RootExt.h"
 #include "HHStatAnalysis/Run2_2016/interface/CommonUncertainties.h"
+#include "HHStatAnalysis/StatModels/interface/StatTools.h"
 
 namespace hh_analysis {
 namespace stat_models {
@@ -45,10 +45,7 @@ void ttbb_base::AddSystematics(ch::CombineHarvester& cb)
 {
     using ch::syst::SystMap;
     using CU = CommonUncertainties;
-    static constexpr size_t DYUncDim = 3;
-    using DYUncMatrix = ROOT::Math::SMatrix<double, DYUncDim, DYUncDim, ROOT::Math::MatRepSym<double, DYUncDim>>;
-    using DYUncVector = ROOT::Math::SVector<double, DYUncDim>;
-    static constexpr double unc_thr = 0.01;
+    static constexpr double unc_thr = 0.005;
 
     CU::lumi().ApplyGlobal(cb, signal_processes, bkg_pure_MC, bkg_TT);
     CU::QCDscale_VV().ApplyGlobal(cb, bkg_VV);
@@ -58,9 +55,9 @@ void ttbb_base::AddSystematics(ch::CombineHarvester& cb)
     CU::scale_j().Apply(cb, all_mc_processes);
     CU::scale_b().ApplyGlobal(cb, signal_processes, bkg_pure_MC, bkg_TT);
 
-    static constexpr double eff_b_unc = 0.03;
-    CU::eff_b().Apply(cb, eff_b_unc, bkg_DY_1b, bkg_VV, bkg_W, bkg_EWK, bkg_ZH);
-    CU::eff_b().Apply(cb, eff_b_unc * std::sqrt(2.), signal_processes, bkg_DY_2b, bkg_TT, bkg_tW);
+    static constexpr double eff_b_unc = 0.02;
+    CU::eff_b().Apply(cb, eff_b_unc, bkg_DY_1b, bkg_VV, bkg_EWK, bkg_ZH, bkg_tW);
+    CU::eff_b().Apply(cb, eff_b_unc * std::sqrt(2.), signal_processes, bkg_DY_2b, bkg_TT);
 
     CU::eff_e().Channel("eTau").Apply(cb, CU::eff_e().up_value, all_mc_processes);
     CU::eff_m().Channel("muTau").Apply(cb, CU::eff_m().up_value, all_mc_processes);
@@ -70,23 +67,34 @@ void ttbb_base::AddSystematics(ch::CombineHarvester& cb)
 
     CU::topPt().Apply(cb, bkg_TT);
 
-    DYUncMatrix dy_unc_cov;
-    dy_unc_cov[0][0] = dy_unc_cov[1][1] = dy_unc_cov[2][2] = 1.0;
-    dy_unc_cov[0][1] = -0.321;
-    dy_unc_cov[0][2] = 0.149;
-    dy_unc_cov[1][2] = -0.449;
+    static constexpr size_t DYUncDim = 3;
+    TMatrixD dy_unc_corr(DYUncDim, DYUncDim);
+    dy_unc_corr[0][0] = dy_unc_corr[1][1] = dy_unc_corr[2][2] = 1.0;
+    dy_unc_corr[0][1] = dy_unc_corr[1][0] = -0.321;
+    dy_unc_corr[0][2] = dy_unc_corr[2][0] = 0.149;
+    dy_unc_corr[1][2] = dy_unc_corr[2][1] = -0.449;
 
-    DYUncVector dy_sf_unc;
+    TVectorD dy_sf_unc(DYUncDim);
     dy_sf_unc[0] = 0.0017;
     dy_sf_unc[1] = 0.016;
     dy_sf_unc[2] = 0.028;
 
+    TVectorD dy_sf(DYUncDim);
+    dy_sf[0] = 1.0472;
+    dy_sf[1] = 1.186;
+    dy_sf[2] = 1.037;
+
+    auto dy_trans_inv = stat_tools::ComputeWhiteningMatrix(dy_unc_corr, dy_sf_unc).Invert();
+
+    std::cout << "ttbb: whitened DY norm covariance matrix" << std::endl;
+    dy_trans_inv.Print();
+
     for(size_t n = 0; n < DYUncDim; ++n) {
         std::ostringstream ss_unc_name;
-        ss_unc_name << "DY" << n << "b_sf";
+        ss_unc_name << "DY_norm_unc_" << n;
         const Uncertainty DY_norm_unc(ss_unc_name.str(), CorrelationRange::Analysis, UncDistributionType::lnN);
         for(size_t k = 0; k < DYUncDim; ++k) {
-            const double unc_value = dy_sf_unc[n] * dy_unc_cov[n][k];
+            const double unc_value = dy_trans_inv[k][n] / dy_sf[k];
             if(std::abs(unc_value) >= unc_thr)
                 DY_norm_unc.Apply(cb, unc_value, bkg_DY.at(k));
         }
