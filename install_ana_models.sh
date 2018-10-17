915ce28cd520a05c1afbedfe1c6ed03af24770f9
Konstantin Androsov <kandrosov@users.noreply.github.com>
2018-10-17 16:34:03 +0200

    Update install_ana_models.sh

--- install_ana_models.sh
+++ install_ana_models.sh
@@ -5,7 +5,7 @@
 mkdir HHStatAnalysis
 cd HHStatAnalysis
 git init
-git remote add cms-hh git@github.com:cms-hh/HHStatAnalysis.git
+git remote add cms-hh https://github.com/cms-hh/HHStatAnalysis.git
 git config core.sparsecheckout true
 echo -e "AnalyticalModels/" >> .git/info/sparse-checkout
 git pull cms-hh master
