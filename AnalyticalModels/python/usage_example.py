dd7af776e0e8b105e3efbcb3dfecdc51d9987276
Fabio Monti <fabio.monti@cern.ch>
2021-03-01 11:41:11 +0100

    analytical NLO model

---
+++ AnalyticalModels/python/usage_example.py
@@ -0,0 +1,47 @@
+# this script provides an example of how to reweight a certain input 
+# sample to a given output benchmark. Two options are viable:
+#
+# 1. If you know the coefficients used to generate the input sample, and,
+#    in case it is a LO Madgraph sample, you have the LO reweightin tool, the 
+#    reweight can be derived as:
+#    reweight = [XS(mHH,costh|outputBM)/XS(mHH,costh|inputBM)] * [XStot(inputBM)/XStot(outputBM)]         
+#
+# 2. If you know the 2D distribution in (mHH,costh) of your input sample Nev(mHH,costh), 
+#    you can derive the reweight as:
+#    reweight = [XS(mHH,costh|outputBM)/Nev(mHH,costh)] * [Nevtot/XStot(outputBM)]
+#    NOTE: the binning for Nev(mHH,costh) has to be the same defined in NonResonantModelNLO        
+#
+# The tools offer also the possibility to reweight only in mHH, 
+# neglecting the dependence on costhetaHH.
+# 
+ 
+import  NonResonantModelNLO
+import ROOT 
+mymodel = NonResonantModelNLO.NonResonantModelNLO()
+mymodel.ReadCoefficients("../data/pm_pw_NLO_Ais_13TeV_V2.txt")
+
+# event example
+event_mHH=514.
+event_costhetaHH=0.3
+event_weight=0.1234
+
+# assume that I want to reweight a given input sample for benchmark XYZ
+# to all the NLO benchmarks using the approach 2 as described above
+inputevfile = ROOT.TFile("inputevdistribution.root")
+histo_Nev = inputevfile.Get("inputev_benchmarkXYZ.root") # this is a TH2 histo
+Nevtot = Nev.Integral() 
+
+
+
+for iBM in range(0,12):
+    BMcouplings = mymodel.getBenchmark(iBM)
+    kl, kt, c2, cg, c2g = BMcouplings[0], BMcouplings[1], BMcouplings[2], BMcouplings[3], BMcouplings[4]
+    print "totalXS", mymodel.getTotalXS(kl, kt, c2, cg, c2g)
+    print "diffXS(event_mHH,event_costhetaHH)", mymodel.getDifferentialXS2D(event_mHH, event_costhetaHH, kl, kt, c2, cg, c2g)
+    print "diffXS(event_mHH)", mymodel.getDifferentialXSmHH(event_mHH, kl, kt, c2, cg, c2g)
+    Nev = histo_Nev.GetBinContent( histo_Nev.FindBin(event_mHH, event_costhetaHH) )
+    XS = mymodel.getDifferentialXS2D(event_mHH, event_costhetaHH, kl, kt, c2, cg, c2g)
+    XStot = mymodel.getTotalXS(kl, kt, c2, cg, c2g)
+    reweight = event_weight * XS/Nev * Nevtot/XStot
+    print reweight
+    print 
