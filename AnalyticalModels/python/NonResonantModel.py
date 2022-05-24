3b7e0d1e47cfb7a14282f002aa58c44598d8ed68
ktht <karlehataht@gmail.com>
2022-05-24 18:31:48 +0300

    Add XS coefficients for 550-600 GeV bins
    Pulled the numbers from publication (Carvalho:2017vnu). Still, the
    reweighting does not quite work as expected.

--- AnalyticalModels/python/NonResonantModel.py
+++ AnalyticalModels/python/NonResonantModel.py
@@ -15,12 +15,12 @@ class NonResonantModel:
         # read coefficients from the input file here
         # to store coefficients use self.
         self.NCostHHbin=4
-        self.NMHHbin=55
+        self.NMHHbin=59
         self.NCoef=15
         self.binGenMHH  = [ 250,260,270,280,290,300,310,320,330,340,
                                350,360,370,380,390,400,410,420,430,440, 
                                450,460,470,480,490,
-                               500,510,520,530,540,550,600,610,620,630,
+                               500,510,520,530,540,550,560,570,580,590,600,610,620,630,
                                640,650,660,670,680,690,700,750,800,850,
                                900,950,1000,1100,1200,1300,1400,1500.,1750,2000,50000]
         self.binGenCostS  = [ 0.0,0.4,0.6,0.8, 1.0 ] #
